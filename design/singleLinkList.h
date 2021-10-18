5cc9b2609df0eb1abaa6ed0bd531cbab32e0dff6
XUE Kaiwen <xuekaiwen94@gmail.com>
2021-10-18 21:15:35 +0800

    Modify-list add erase/size()

--- design/singleLinkList.h
+++ design/singleLinkList.h
@@ -18,7 +18,7 @@ template <class T>
 class Node
 {
 private:
-    Node(const T & x): val(x), next(0) {}
+    Node(const T & x): val(x), next(nullptr) {}
     T val;
     Node<T> * next;
     friend class List<T>;
@@ -60,7 +60,7 @@ class List
 {
 public:
     using iterator = Iterator<T>;
-    List() : head(nullptr), last(nullptr){ };
+    List() : head(nullptr), last(nullptr), m_size(0){ };
     ~List(){
         Node<T> * p = head;
         while (p)
@@ -69,6 +69,7 @@ public:
             delete p;
             p = next;
         }
+        m_size = 0;
     };
 
     void add(const T & x){
@@ -80,17 +81,44 @@ public:
             last = newNode;
         }
         last->next = nullptr;  //the end state
+        m_size++;
     }
+
+    void erase(const T& x){
+
+        if(head->val == x){
+            Node<T>* tmp = head;
+            head = head->next;
+            tmp->next = nullptr;
+            delete(tmp);
+            m_size--;
+            return;
+        }
+
+        Node<T>* p = head;
+        while(p && p->next && p->next->val != x) p = p->next;
+        if(!p->next) return; //didn't find
+        Node<T>* tmp = p->next;
+        p->next = p->next->next;
+        tmp->next = nullptr;
+        delete(tmp);
+        m_size--;
+    }
+
     iterator begin() const{
         return iterator(head);
     }
     iterator end() const{
         return iterator(nullptr);
     }
+    unsigned size() const{
+        return m_size;
+    }
 
 protected:
     Node<T> * head;
     Node<T> * last;
+    unsigned m_size;
 };
 
 void testList(){
@@ -98,32 +126,53 @@ void testList(){
 
     cout << "test empty list: " << endl;
     for(auto it = ml.begin(); it != ml.end(); it++) cout << *it << endl;
-    cout << endl;
+    cout << ml.size() << endl;
 
     ml.add(1);
     cout << "test one value" << endl;
     for(auto it = ml.begin(); it != ml.end(); it++) cout << *it << endl;
-    cout << endl;
+    cout << ml.size() << endl;
 
     ml.add(2);
     cout << "test two value" << endl;
     for(auto it = ml.begin(); it != ml.end(); it++) cout << *it << endl;
-    cout << endl;
+    cout << ml.size() << endl;
 
     ml.add(3);
     cout << "test three value" << endl;
     for(auto it = ml.begin(); it != ml.end(); it++) cout << *it << endl;
-    cout << endl;
+    cout << ml.size() << endl;
 
     ml.add(4);
     cout << "test four value" << endl;
     for(auto it = ml.begin(); it != ml.end(); it++) cout << *it << endl;
-    cout << endl;
+    cout << ml.size() << endl;
 
     //test ranged-based loop
     cout << "ranged-based loop" << endl;
-    for(auto v : ml)
-        cout << v << endl;
+    for(auto v : ml) cout << v << endl;
+    cout << ml.size() << endl;
+
+    cout << "test erase" << endl;
+    ml.erase(5);
+    for(auto v : ml) cout << v << endl;
+    cout << ml.size() << endl;
+
+    cout << "test erase" << endl;
+    ml.erase(1);
+    for(auto v : ml) cout << v << endl;
+    cout << ml.size() << endl;
+
+    cout << "test erase" << endl;
+    ml.erase(3);
+    for(auto v : ml) cout << v << endl;
+    cout << ml.size() << endl;
+
+    cout << "test erase" << endl;
+    ml.erase(4);
+    for(auto v : ml) cout << v << endl;
+    cout << ml.size() << endl;
+
 }
 
 #endif //LEETCODE_SINGLELINKLIST_H
--- main.cpp
+++ main.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 
 int main(){
-    ConsecutiveNumbersSum_829 cns;
-    cns.test();
+    //List<int> l;
+    testList();
     return 0;
 }
